a6d2e9a4f989e43bdd3756bbc702c5ddec78540c
Jammy-ez <76751003+Jammy-ez@users.noreply.github.com>
2021-11-29 20:03:20 +0000

    Update setup.bat

--- nuke bot/setup.bat
+++ nuke bot/setup.bat
@@ -1,3 +1,7 @@
 py -m pip install numpy
 py -m pip install discord
 py -m pip install aiohttp
+py -m pip install colorama
+py -m pip install datetime
+py -m pip install discum
+py -m pip install pycord
